819c1b1df02fd4fdd573362b0ceb11579d12cdb7
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-01-07 20:46:22 -0500

    v3

---
+++ Activity3.py
@@ -0,0 +1,13 @@
+print("enter the marks obtained of four subjects:")
+
+maths= int(input("maths:"))
+science= int(input("science:"))
+english= int(input("english:"))
+Hindi= int(input("Hindi:"))
+
+sum= maths + science + english + Hindi
+print ("Sum of maths, science, english and Hindi is:")
+
+perc= (sum/400) * 100
+print(end = "percentage is:")
+print(perc)
